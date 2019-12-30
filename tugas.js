22aa27b72c083cc2ea8274d545e38d988bed43a6
diahml <diahmartina16@gmail.com>
2019-12-30 16:07:44 +0700

    second-commit

--- tugas.js
+++ tugas.js
@@ -1,5 +1,16 @@
-for(i=1; i<=5;i++){
+var a ="*"
+for (let i = 1; i <= 5; i++) {
+    let data =[]
+    for (let j = 1; j <= i; j++){
+        data.push(a)
+  }console.log(data.join(" "))
+}
+
+
+for(i=1; i<=10;i++){
    
     console.log("1 x "+i+" ="+ 1*i)
     
 }
+
+  
